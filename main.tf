cb3d18699f5601119819530d6886fa84bac1c71d
Joe Schoonover <joe@fluidnumerics.com>
2022-06-27 13:26:43 -0600

    Add cluster name to private ip address
    * This allows users to spin up a second cluster in the same project

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+# Copyright 2022 Fluid Numerics LLC
+#
+# > Modified for use with open source lustre gcp
+# > Modified for use with dev/ops style deployment
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 # Copyright 2019 Google LLC
 #
@@ -51,7 +67,7 @@ data "google_compute_subnetwork" "slurm_subnet" {
 resource "google_compute_global_address" "private_ip_address" {
   count = var.cloudsql_slurmdb ? 1 : 0
   provider = google-beta
-  name = "private-ip-address"
+  name = "${var.cluster_name}-private-ip-address"
   purpose = "VPC_PEERING"
   address_type = "INTERNAL"
   prefix_length = 16
